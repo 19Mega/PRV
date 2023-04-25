a258623b1846eb42addbc8e9579121b7e63caf68
Matias <matiasborba89@gmail.com>
2023-04-25 18:27:41 -0300

    main window size modified

--- PRV_main.py
+++ PRV_main.py
@@ -5,22 +5,18 @@ import tkinter as tk
 #1c1a2e
 
 root = tk.Tk()
-root.geometry("900x750")
+root.geometry("1200x750")
 root.resizable(False, False) 
 
-frame0 = tk.Frame(root, bg="black", width=900, height=650)
+frame0 = tk.Frame(root, bg="black", width=1200, height=750)
 frame0.pack(fill=tk.X)
 
 # Sección 1
-frame1 = tk.Frame(frame0, bg="#312f47", width=650,height=680)
+frame1 = tk.Frame(frame0, bg="#312f47", width=700,height=750)
 frame1.pack(fill=tk.X, side="right")
 
 # Sección 2
-frame2 = tk.Frame(frame0, bg="#3f3e52",width=250, height=680)
+frame2 = tk.Frame(frame0, bg="#3f3e52",width=500, height=750)
 frame2.pack(fill=tk.X, side="left")
-
-# Sección 3
-frame3 = tk.Frame(root, bg="#1c1a2e", height=70)
-frame3.pack(fill=tk.X)
 
 root.mainloop()
